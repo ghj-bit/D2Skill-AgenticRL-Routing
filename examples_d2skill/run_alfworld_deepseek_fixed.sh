2a90ec8579f0825fa3619bd290b5fc4a8324e7bf
ghj-bit <ghj948027@gmail.com>
2026-04-23 19:48:36 +0800

    0423

--- examples_d2skill/run_alfworld_deepseek_fixed.sh
+++ examples_d2skill/run_alfworld_deepseek_fixed.sh
@@ -14,9 +14,9 @@ export COST_COE="${COST_COE:-0.0}"
 bash "${SCRIPT_DIR}/run_alfworld_d2skill.sh" "$ENGINE" \
     routing.force_model_enable=True \
     routing.force_model_name="$FIXED_ROUTE_MODEL" \
-    +env.skills_only_memory.enable_dynamic_update=True \
-    +env.skills_only_memory.update_source=validation \
-    +env.skills_only_memory.update_save_traj=True \
+    env.skills_only_memory.enable_dynamic_update=True \
+    env.skills_only_memory.update_source=validation \
+    env.skills_only_memory.update_save_traj=True \
     trainer.val_only=True \
     trainer.val_before_train=True \
     trainer.project_name='verl_agent_alfworld_fixed_route' \
