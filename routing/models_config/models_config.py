cc2d96b5d88e9fa4a8eba56f30d6c03c340aa212
ghj-bit <ghj948027@gmail.com>
2026-05-05 12:29:00 +0800

    26_05_05_4

--- routing/models_config/models_config.py
+++ routing/models_config/models_config.py
@@ -1,6 +1,6 @@
 MODEL_CONF = {
     "qwen2.5-7B": {
-        "api_base": "https://notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/3a8e9a70-c91e-459d-ad61-e9b54493df6c/95d65709-a2dc-4c6b-a8bb-15ef7bec8375/proxy/8042/v1",
+        "api_base": "https://notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/3a8e9a70-c91e-459d-ad61-e9b54493df6c/30048e5a-fbe7-44d7-b103-ef597e2fc210/proxy/8042/v1",
         "api_key": "empty"
     },
     "deepseek": {
