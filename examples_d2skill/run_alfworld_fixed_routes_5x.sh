c54a4c30ff01ae87809d2c21a0b27386d50a5de6
ghj-bit <ghj948027@gmail.com>
2026-05-19 21:03:53 +0800

    fixed_refine_1

--- examples_d2skill/run_alfworld_fixed_routes_5x.sh
+++ examples_d2skill/run_alfworld_fixed_routes_5x.sh
@@ -81,8 +81,8 @@ run_one_model() {
     done
 }
 
+run_one_model "gemma-2-27B" "${SCRIPT_DIR}/run_alfworld_gemma_2_27b_fixed.sh" "$@"
 run_one_model "deepseek" "${SCRIPT_DIR}/run_alfworld_deepseek_fixed.sh" "$@"
 run_one_model "qwen3-8B" "${SCRIPT_DIR}/run_alfworld_qwen3_8b_fixed.sh" "$@"
-run_one_model "gemma-2-27B" "${SCRIPT_DIR}/run_alfworld_gemma_2_27b_fixed.sh" "$@"
 
 python3 "${SCRIPT_DIR}/aggregate_fixed_route_metrics.py" "$LOG_ROOT"
