e1594fe5570a085fc9b84de07f5659959440f2a5
ghj-bit <ghj948027@gmail.com>
2026-06-04 22:21:29 +0800

    0604_11

--- routing/models_config/models_config.py
+++ routing/models_config/models_config.py
@@ -8,7 +8,7 @@ MODEL_CONF = {
         "api_key": "empty"
     },
     "qwen3-30B": {
-        "api_base": "https://ai-notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/6885e439-7002-4233-b0fc-46dc16ae00eb/9f66a1e3-346d-4718-9450-bc0074d4f3ff/proxy/8042/v1",
+        "api_base": "https://ai-notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/6885e439-7002-4233-b0fc-46dc16ae00eb/112d74f9-5c92-457c-98f6-3f3b230f9ed4/proxy/8042/v1",
         "api_key": "empty"
     }
 }
