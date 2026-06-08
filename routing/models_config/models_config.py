8fc9ecf5194dbf5b47994fedf75e826d8f8f8313
ghj-bit <ghj948027@gmail.com>
2026-06-08 13:21:25 +0800

    0608_change_api

--- routing/models_config/models_config.py
+++ routing/models_config/models_config.py
@@ -4,11 +4,11 @@ MODEL_CONF = {
         "api_key": "empty"
     },
     "deepseek": {
-        "api_base": "https://ai-notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/4ff709dd-915e-4392-8a69-12c61dc95edb/adacd345-5b66-42d3-a3a3-94234c2aa3cd/proxy/8055/v1",
+        "api_base": "https://ai-notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/4ff709dd-915e-4392-8a69-12c61dc95edb/ae01f99f-3457-4c25-a7ce-ed967ad2ff02/proxy/8055/v1",
         "api_key": "empty"
     },
     "qwen3-30B": {
-        "api_base": "https://ai-notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/6885e439-7002-4233-b0fc-46dc16ae00eb/c41b9e41-8eb4-48e7-9881-9007a7f3b3a3/proxy/8042/v1",
+        "api_base": "https://ai-notebook-inspire.sii.edu.cn/ws-9dcc0e1f-80a4-4af2-bc2f-0e352e7b17e6/project-b795c114-135a-40db-b3d0-19b60f25237b/user-543feed4-0be2-4972-8987-a324af06c93f/vscode/6885e439-7002-4233-b0fc-46dc16ae00eb/1dc830a1-9cea-4c7a-ada4-65a560ea4921/proxy/8042/v1",
         "api_key": "empty"
     }
 }
